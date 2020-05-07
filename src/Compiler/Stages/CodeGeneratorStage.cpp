4b11925b75c8e4f476121295e1e876b33f2e487d
lionjove <33174402+lionjove@users.noreply.github.com>
2020-05-07 22:20:28 +0700

    ASM project template

--- src/Compiler/Stages/CodeGeneratorStage.cpp
+++ src/Compiler/Stages/CodeGeneratorStage.cpp
@@ -1,10 +1,60 @@
 #include "stdafx.h"
 #include "CodeGeneratorStage.h"
 
-void CodeGeneratorStage::DoStage()
+
+#include "ParserElements/ExpressionStatement.h"
+#include "ParserElements/Function.h"
+#include "ParserElements/If.h"
+#include "ParserElements/Let.h"
+#include "ParserElements/Scope.h"
+#include "ParserElements/While.h"
+
+void CodeGeneratorStage::Notify(std::pair<std::shared_ptr<AbstractTreeNode>, std::shared_ptr<IdentificatorTable>> data)
+{
+	m_table = data.second;
+
+	
+}
+
+void CodeGeneratorStage::TranslateNode(std::shared_ptr<AbstractTreeNode> node)
+{
+	auto let = std::dynamic_pointer_cast<Let>(node);
+	auto expressionStatement = std::dynamic_pointer_cast<ExpressionStatement>(node);
+	auto ifElem = std::dynamic_pointer_cast<If>(node);
+	auto whileElem = std::dynamic_pointer_cast<While>(node);
+	auto scope = std::dynamic_pointer_cast<Scope>(node);
+	auto function = std::dynamic_pointer_cast<Function>(node);
+
+	if (let != nullptr)
+		TranslateLet(let);
+	else if (expressionStatement != nullptr)
+		TranslateExpressionStatement(expressionStatement);
+	else if (ifElem != nullptr)
+		TranslateIfElement(ifElem);
+	else if (whileElem != nullptr)
+		TranslateWhileElement(whileElem);
+	else if (scope != nullptr)
+		for (auto child : scope->m_childs) TranslateNode(child);
+	else if (function != nullptr)
+		TranslateFunction(function);
+}
+
+void CodeGeneratorStage::TranslateLet(std::shared_ptr<Let> node)
 {
 }
 
-void CodeGeneratorStage::Notify(std::pair<std::shared_ptr<AbstractTreeNode>, std::shared_ptr<IdentificatorTable>> data)
+void CodeGeneratorStage::TranslateExpressionStatement(std::shared_ptr<ExpressionStatement> node)
+{
+}
+
+void CodeGeneratorStage::TranslateIfElement(std::shared_ptr<If> node)
+{
+}
+
+void CodeGeneratorStage::TranslateWhileElement(std::shared_ptr<While> node)
+{
+}
+
+void CodeGeneratorStage::TranslateFunction(std::shared_ptr<Function> node)
 {
 }
--- src/Compiler/Stages/CodeGeneratorStage.h
+++ src/Compiler/Stages/CodeGeneratorStage.h
@@ -4,11 +4,17 @@
 #include "CompilerParts/Observers/IObserver.h"
 #include "CompilerParts/StageOutputStructs.h"
 
+class Let;
+class ExpressionStatement;
+class If;
+class While;
+class Function;
+
 class __declspec(dllexport) CodeGeneratorStage : public IStage, public IObserver<std::pair<std::shared_ptr<AbstractTreeNode>, std::shared_ptr<IdentificatorTable>>>
 {
 public:
 	/// IStage override
-	void DoStage() override;
+	void DoStage() override {}
 
 	[[nodiscard]] std::wstring GetStageName() override { return L"CodeGenerator"; }
 	///
@@ -16,4 +22,24 @@ public:
 	/// INewParserTreeObserver override
 	void Notify(std::pair<std::shared_ptr<AbstractTreeNode>, std::shared_ptr<IdentificatorTable>> data) override;
 	///
+
+private:
+
+	enum class Registers
+	{
+		AX,
+		BX,
+		CX,
+		DX
+	};
+
+	void TranslateNode(std::shared_ptr<AbstractTreeNode> node);
+
+	void TranslateLet(std::shared_ptr<Let> node);
+	void TranslateExpressionStatement(std::shared_ptr<ExpressionStatement> node);
+	void TranslateIfElement(std::shared_ptr<If> node);
+	void TranslateWhileElement(std::shared_ptr<While> node);
+	void TranslateFunction(std::shared_ptr<Function> node);
+	
+	std::shared_ptr<IdentificatorTable> m_table;
 };
